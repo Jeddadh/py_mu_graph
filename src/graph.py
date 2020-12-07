8a4fb0318b454791eb8e2ba21e56e414c962d2f9
hjeddad <jeddad.hamza@gmail.com>
2020-12-07 16:45:15 +0100

    create a first classes and some basic methods

---
+++ src/graph.py
@@ -0,0 +1,50 @@
+class Node(object):
+    node_id = 0
+
+    def __init__(self):
+        self.__id = Node.node_id
+        Node.node_id += 1
+        self.__adj = set()
+
+    @property
+    def id(self) -> int:
+        """The node's id"""
+        return self.__id
+
+    @property
+    def adj(self) -> set:
+        """
+        The list of adjacent nodes
+        :return: set : the list of adjacent nodes
+        """
+        return self.__adj
+
+    def add_adj(self, node):
+        """
+        Add a node to the adjacent nodes set
+        :param node: of type Node
+        """
+        if not isinstance(node, Node):
+            raise TypeError("node must be an instance of Node")
+        self.__adj.add(node.id)
+
+    def remove_adj(self, node):
+        """
+        remove a node from adjacent nodes set. Raise a KeyError if the node does not exist.
+        :param node: of type Node
+        :return:
+        """
+        if not isinstance(node, Node):
+            raise TypeError("node must be an instance of Node")
+        try:
+            self.__adj.remove(node.id)
+        except KeyError:
+            raise KeyError("node must be an adjacent node")
+
+
+class Graph(object):
+    def __init__(self):
+        self.__nodes_id = set()
+
+    def add_node(self, node: Node):
+        self.__nodes_id.append(node.id)
